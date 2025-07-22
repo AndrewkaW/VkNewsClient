171fc7931795d9afa2c4c1bbb8b48afe741da441
Andrewka <Uzzbagoyzza@yandex.ru>
2025-07-23 01:06:13 +0300

    Практика с элементами material components.

--- app/src/main/java/com/example/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/MainActivity.kt
@@ -1,15 +1,35 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.vknewsclient
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.vknewsclient.ui.theme.PreviewVkPostLite
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.vknewsclient.ui.theme.VkNewsClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,14 +38,78 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VkNewsClientTheme {
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .background(MaterialTheme.colorScheme.background)
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
                 ) {
-                   PreviewVkPostLite()
+                    //     PreviewVkPostLite()
+                    Test3()
                 }
             }
         }
     }
 }
 
+@Composable
+private fun Test() {
+    OutlinedButton(
+        onClick = {},
+    ) {
+        Text(
+            text = "Hello World"
+        )
+    }
+}
+
+@Composable
+private fun Test2() {
+    TextField(value = "Value", onValueChange = {}, label = { Text(text = "Label") }
+    )
+}
+
+
+@Composable
+private fun Test3() {
+    BasicAlertDialog(
+        onDismissRequest = {}
+    ) {
+        Surface(
+            modifier = Modifier
+                .wrapContentWidth()
+                .wrapContentHeight(),
+            shape = MaterialTheme.shapes.large,
+            tonalElevation = AlertDialogDefaults.TonalElevation,
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = "Are you sure?",
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily.Serif,
+                )
+
+                Text(
+                    text =
+                        "Do you want delete this file?."
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                Row {
+                    TextButton(
+                        onClick = { },
+                    ) {
+                        Text(text = "No", color = MaterialTheme.colorScheme.onPrimary)
+                    }
+                    TextButton(
+                        onClick = { },
+                    ) {
+
+                        Text(text = "Yes", color = MaterialTheme.colorScheme.onPrimary)
+                    }
+                }
+
+
+            }
+        }
+
+    }
+}
