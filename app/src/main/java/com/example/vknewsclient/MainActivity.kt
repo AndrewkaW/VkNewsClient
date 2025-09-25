ea42266ba970695e7cb93850c7b35783f5fcd2f0
Andrewka <Uzzbagoyzza@yandex.ru>
2025-09-25 23:01:20 +0300

    Реализовал авторизацию через VKID

--- app/src/main/java/com/example/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/MainActivity.kt
@@ -1,47 +1,24 @@
 package com.example.vknewsclient
 
 import android.os.Bundle
-import android.util.Log
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.vknewsclient.ui.theme.AuthState
 import com.example.vknewsclient.ui.theme.LoginScreen
 import com.example.vknewsclient.ui.theme.MainScreen
-import com.example.vknewsclient.ui.theme.MyNumber
-import com.example.vknewsclient.ui.theme.SideEffectTest
 import com.example.vknewsclient.ui.theme.VkNewsClientTheme
-import com.vk.api.sdk.VK
-import com.vk.api.sdk.auth.VKAuthenticationResult
-import com.vk.api.sdk.auth.VKScope
+import com.vk.id.VKID
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             VkNewsClientTheme {
                 val viewModel: MainViewModel = viewModel<MainViewModel>()
                 val authState = viewModel.authState.observeAsState(AuthState.Initial)
-                val launcher =
-                    rememberLauncherForActivityResult(contract = VK.getVKAuthActivityResultContract()) {
-                        viewModel.performAuthResult(it)
-                    }
 
                 when (authState.value) {
                     AuthState.Authorized -> {
@@ -49,7 +26,12 @@ class MainActivity : ComponentActivity() {
                     }
 
                     AuthState.NotAuthorized -> {
-                        LoginScreen { launcher.launch(input = listOf(VKScope.WALL)) }
+                        LoginScreen {
+                            VKID.instance.authorize(
+                                this@MainActivity,
+                                viewModel.vkAuthCallback
+                            )
+                        }
                     }
 
                     AuthState.Initial -> {}
--- app/src/main/java/com/example/vknewsclient/MainViewModel.kt
+++ app/src/main/java/com/example/vknewsclient/MainViewModel.kt
@@ -5,29 +5,35 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.vknewsclient.ui.theme.AuthState
-import com.vk.api.sdk.VK
-import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.id.AccessToken
+import com.vk.id.VKIDAuthFail
+import com.vk.id.auth.VKIDAuthCallback
 
 class MainViewModel() : ViewModel() {
 
     private val _authState = MutableLiveData<AuthState>(AuthState.Initial)
     val authState: LiveData<AuthState> = _authState
 
+    private var token : String? = null
+
     init {
-        _authState.value = if (VK.isLoggedIn()) {
+        _authState.value = if (!token.isNullOrBlank()) {
             AuthState.Authorized
         } else {
             AuthState.NotAuthorized
         }
     }
 
-    fun performAuthResult(result: VKAuthenticationResult) {
-        if (result is VKAuthenticationResult.Success) {
+    val vkAuthCallback = object : VKIDAuthCallback {
+        override fun onAuth(accessToken: AccessToken) {
+            token = accessToken.token
             _authState.value = AuthState.Authorized
-        } else {
-            val authErrorMassage = (result as VKAuthenticationResult.Failed).exception.authError
-            Log.e("MainViewModel", "Auth Failed : $authErrorMassage")
+            Log.i("MainViewModel", "Auth Access : ${accessToken.userData}")
+        }
+
+        override fun onFail(fail: VKIDAuthFail) {
             _authState.value = AuthState.NotAuthorized
+            Log.e("MainViewModel", "Auth Failed : ${fail.description}")
         }
     }
 }
